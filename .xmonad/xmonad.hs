78ad2b9683a96ae7978aefcb455f873b2c5f09e7
Ahmad Samiei <ahmad.samiei@gmail.com>
2016-08-23 19:28:27 +0800

    Add key mapping for toggle fullscreen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,17 +1,17 @@
+import XMonad
 import System.IO
 import System.Exit
-import XMonad
 import XMonad.Config.Xfce
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Fullscreen
-import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
-import XMonad.Config.Gnome
-import XMonad.Layout.NoBorders
+import XMonad.Layout.ToggleLayouts
 import XMonad.Util.EZConfig(additionalKeys)
-
 
 myBorderWidth = 2
 
@@ -19,13 +19,14 @@ myBorderWidth = 2
 main = xmonad $ gnomeConfig {
     modMask = mod4Mask
   , borderWidth = 2
-  , layoutHook = smartBorders $ layoutHook gnomeConfig
+  , layoutHook = smartBorders $ toggleLayouts Full $ layoutHook gnomeConfig
   , terminal = "gnome-terminal"
   , startupHook = myStartupHook
   , focusFollowsMouse = False
 } `additionalKeys`
     [
         ((mod4Mask .|. controlMask, xK_l), spawn "gnome-screensaver-command --lock")
+      , ((mod4Mask, xK_f), sendMessage (Toggle "Full"))
     ]
 
 myStartupHook = do
@@ -35,4 +36,3 @@ myStartupHook = do
     spawn "nm-applet &"
     spawn "synapse --startup"
     spawn "export _JAVA_AWT_WM_NONREPARENTING=1"
-
