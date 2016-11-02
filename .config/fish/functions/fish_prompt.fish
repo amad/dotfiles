78605669e5ffadd827e1299de3ef3677f932141a
Ahmad Samiei <ahmad.samiei@gmail.com>
2016-11-02 10:42:41 +0800

    Update prompt colors
    fancy!

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,12 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
+  set -l last_status $status
 
   # User
-  set_color normal
+  set_color $fish_color_user
   echo -n (whoami)
+  set_color normal
   echo -n '@'
+  set_color $fish_color_host
   echo -n (hostname -s)
   set_color normal
 
@@ -17,8 +19,9 @@ function fish_prompt --description 'Write out the prompt'
   echo -n (pwd)
   set_color normal
 
-  set_color $fish_color_status
+  set_color $fish_color_history_current
   __fish_git_prompt
+  #__terlar_git_prompt
   __fish_hg_prompt
   echo
   set_color normal
