95d4149a6cc0a06f1a53483a6e24d57480d8c879
Ahmad Samiei <ahmad.samiei@gmail.com>
2016-11-02 12:16:19 +0800

    Added git status in prompt message

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -19,12 +19,12 @@ function fish_prompt --description 'Write out the prompt'
   echo -n (pwd)
   set_color normal
 
-  set_color $fish_color_history_current
-  __fish_git_prompt
-  #__terlar_git_prompt
-  __fish_hg_prompt
-  echo
+  echo -n ' '
+  #__fish_git_prompt
+  #__fish_hg_prompt
+  my_git_prompt
   set_color normal
+  echo
 
   if not test $last_status -eq 0
     set_color $fish_color_error
