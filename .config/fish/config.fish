df6d5df1c23557ac42af17b288e311f9bd0da187
Ahmad Samiei <ahmad.samiei@gmail.com>
2017-11-19 20:18:17 +0100

    Set idekey for xdebug everywhere

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
 source ~/.bash_aliases
 alias `~`='cd ~'
-set -g -x fish_greeting ''
+set -gx fish_greeting ''
 set -gx PATH $PATH $HOME/bin
 set -gx PATH $PATH $HOME/.npm-global/bin
 set -gx PATH $PATH $HOME/.composer/vendor/bin
 set -gx PATH $PATH $HOME/.npm-global/bin
 set -gx PATH $PATH /usr/local/opt/node@6/bin
 set -gx PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
-
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -gx fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -gx XDEBUG_CONFIG 'idekey=CODE'
 
 if [ "$TERM_PROGRAM" = "Apple_Terminal" ]
   fortune -s -n 100 -e computers | cowsay -f default | lolcat
