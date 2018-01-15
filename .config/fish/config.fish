e24659037b052c84131d681c7e5d587af6701f9f
Ahmad Samiei <ahmad.samiei@gmail.com>
2018-01-15 21:35:52 +0100

    Set GOPATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,11 @@ set -gx PATH $PATH $HOME/.composer/vendor/bin
 set -gx PATH $PATH /usr/local/opt/node@6/bin
 set -gx PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
 set -gx fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -gx PATH $PATH $HOME/Go/bin
+set -gx PATH $PATH /usr/local/opt/go/libexec/bin
 set -gx XDEBUG_CONFIG 'idekey=CODE'
+set -gx GOPATH $HOME/Go
+set -gx GOROOT /usr/local/opt/go/libexec
 
 if [ "$TERM_PROGRAM" = "Apple_Terminal" ]
   fortune -s -n 100 -e computers | cowsay -f default | lolcat
