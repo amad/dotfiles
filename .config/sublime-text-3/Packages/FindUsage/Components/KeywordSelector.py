fd9e19dd01d912ba307a8ea7c389dd67032ba6f0
Ahmad Samiei <ahmad.samiei@gmail.com>
2024-03-02 09:01:35 +0100

    Delete old configs

--- .config/sublime-text-3/Packages/FindUsage/Components/KeywordSelector.py
+++
@@ -1,8 +0,0 @@
-class KeywordSelector:
-    def execute(view):
-        keyword = ''
-        for region in view.sel():
-            word = view.word(region) if region.begin() == region.end() else region
-            if not word.empty():
-                keyword = view.substr(word)
-        return keyword if keyword.isalnum() else ''
