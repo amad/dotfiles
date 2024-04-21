b2f4190a077414f4f57cb808378f5c1bde25dff5
Ahmad Samiei <ahmad.samiei@gmail.com>
2024-04-21 10:15:18 +0200

    Update nvchad mappings to unmap ;

--- .config/nvchad-exp/custom/mappings.lua
+++ .config/nvchad-exp/custom/mappings.lua
@@ -3,7 +3,7 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
 
     --  format with conform
     ["<leader>fm"] = {
